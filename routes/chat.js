a7a846b003eaac59bcb211dc00c1067d7faacf15
Szanyi Krisztián <szkript@gmail.com>
2020-06-23 12:51:47 +0200

    minimalistic chat added

---
+++ routes/chat.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', function (req, res, next) {
+    let msg = 'nani';
+    // res.send('<h1>Hello world</h1>');
+    res.render('chat', {
+        title: 'chat',
+        result: msg
+    });
+});
+
+module.exports = router;
