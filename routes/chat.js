19280359544a5c98bbd0b070312208a8f8660aeb
Szanyi Krisztián <szkript@gmail.com>
2020-06-26 13:33:02 +0200

    grid implementation first steps

--- routes/chat.js
+++ routes/chat.js
@@ -3,12 +3,9 @@ const router = express.Router();
 
 
 router.get('/', function (req, res, next) {
-    let msg = 'nani';
     // res.send('<h1>Hello world</h1>');
-
     res.render('chat', {
         title: 'chat',
-        result: msg,
     });
 });
 
